f0fd973cbd2cf7e5fb8e799335cef8e6c93814c1
“Your <03anusmitakundu@gmail.com>
2023-07-28 14:07:24 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,19 @@
+
+    function appendToDisplay(value) {
+        document.getElementById('display').value += value;
+    }
+
+    function clearDisplay() {
+        document.getElementById('display').value = '';
+    }
+
+    function backspace() {
+        let display = document.getElementById('display');
+        display.value = display.value.slice(0, -1);
+    }
+
+    function evaluate() {
+        let display = document.getElementById('display');
+        display.value = eval(display.value);
+    }
+
